28d9a9bc4e24287ab6e3a24dcb8e7e44e61de424
The0nix <nektonikto999@gmail.com>
2017-07-16 17:22:49 +0300

    First commit. Can install and configure whatsapp

---
+++ whatsapp/configure.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+from subprocess import Popen, PIPE
+import json
+import os
+import sys
+
+print('Configuring whatsapp messenger')
+
+os.chdir('yowsup')
+cc = input('Input country code [7]: ').strip() or '7'
+mcc = input('Input mobile country code [205]: ').strip() or '205'
+phone = ''
+while phone == '':
+    phone = input('Input phone number: ').strip()
+mnc = input('Input operator code (01 - MTS, 02 - Megaphone, 20 - Tele2, 99 - Biline) [01]: ').strip() or '01'
+
+if os.system(sys.executable + ' ./yowsup-cli registration --requestcode sms --phone ' + phone + ' --cc ' + cc + ' --mcc ' + mcc + ' --mnc ' + mnc) != 0:
+    exit('Error while registering using yowsup-cli')
+
+code = ''
+while (code == ''):
+    code = input('Input confirmation code (XXX-XXX): ').strip()
+
+with Popen([sys.executable, './yowsup-cli', 'registration', '--register', code, '--phone', phone, '--cc', cc], stderr=PIPE, stdout=PIPE) as proc:
+    if proc.wait(60) != 0:
+        exit('Error while confirming phone number using yowsup-cli')
+    data = proc.stderr.read()
+
+parsed = json.loads(data.decode('utf-8')[data.find(b'{'):data.find(b'}') + 1]);
+password = parsed['pw']
+
+os.chdir('..')
+confdict = {'phone': phone, 'password': password}
+with open('config.json', 'w') as config:
+    config.write(json.dumps(confdict, indent=4))
+
+print('Successfully configured whatsapp messenger')
---
+++ whatsapp/msg.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./yowsup./yowsup-cli demos -c yowsup-cli.config -M -s $1 $2
