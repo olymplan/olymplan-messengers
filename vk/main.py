2a754ae9048c584bf22b27a04dfd176fedbb194b
Ubuntu <ubuntu@ip-172-31-37-90.eu-central-1.compute.internal>
2017-08-05 07:50:43 +0000

    add twisted, add log function, fix env vars

--- vk/main.py
+++ vk/main.py
@@ -3,9 +3,14 @@ from flask import request
 import requests
 import json
 import sys
+import os
+import datetime
 app = Flask(__name__)
 TOKEN = os.environ['OLYMPLAN_VK_TOKEN']
 CONFIRMATION_TOKEN = os.environ['OLYMPLAN_VK_CTOKEN']
+
+def log(string):
+    print('[{}] {}'.format(str(datetime.datetime.now()), string), file=sys.stderr)
 
 def method(name, params, token):
     response = requests.get('https://api.vk.com/method/' + name, params={**params, 'access_token' : token})
@@ -14,7 +19,7 @@ def method(name, params, token):
 @app.route('/', methods=['POST'])
 def process():
     # Распаковываем json из пришедшего GET-запроса
-    print(request.data, file=sys.stderr)
+    log(request.data)
     data = json.loads(request.data.decode('utf-8'))
     # Вконтакте в своих запросах всегда отправляет поле типа
     if 'type' not in data.keys():
@@ -32,12 +37,27 @@ def process():
                 method('messages.send', {'user_id': data['object']['user_id'],
                                         'message': contests}, TOKEN)
             except requests.exceptions.Timeout as e:
-                print('No user {} in api'.format(domain), file=sys.stderr)
+                log('No user {} in api'.format(domain))
                 method('messages.send', {'user_id': data['object']['user_id'],
                                         'message': 'Упс! Что-то пошло не так. Вы точно зарегестрированы на сайте с ником "{}"?'.format(domain)}, TOKEN)
         else:
             method('messages.send', {'user_id' : data['object']['user_id'], 'message': 'Привет, это Олимплан! Чтобы посмотреть своё расписание напишите "Расписание"'}, TOKEN)
         return 'ok'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1337)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=1337)
+
+if __name__ == "__main__":
+    reactor_args = {}
+    
+    def run_twisted_wsgi():
+        from twisted.internet import reactor
+        from twisted.web.server import Site
+        from twisted.web.wsgi import WSGIResource
+
+        resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+        site = Site(resource)
+        reactor.listenTCP(1337, site, interface='0.0.0.0')
+        reactor.run(**reactor_args)
+        
+    run_twisted_wsgi()
